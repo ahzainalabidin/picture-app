f2bf4e6efe62ce97915a479a673d563f415a3d5a
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-12 11:04:23 +0800

    Handle form input using state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import searchImages from './api';
+import SearchBar from './components/SearchBar';
+
 function App() {
+
+  const handleSubmit = (term) => {
+    console.log('Do a search with', term);
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
+    </div>
   );
+
 }
 
 export default App;
