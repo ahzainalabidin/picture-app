f9e2105d66a377c7e2c23b0146285ba06febf721
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-12 12:29:01 +0800

    Add styling to images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,3 +1,4 @@
+import './ImageList.css';
 import ImageShow from './ImageShow';
 
 function ImageList({ images }) {
@@ -7,7 +8,7 @@ function ImageList({ images }) {
     });
 
     return (
-        <div>{renderedImages}</div>
+        <div className='image-list'>{renderedImages}</div>
     );
 
 }
